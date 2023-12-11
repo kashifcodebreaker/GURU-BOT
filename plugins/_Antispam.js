587e3d261f1c01bdfa5e164d87ecf330aa9765b6
kashifcodebreaker <141224408+kashifcodebreaker@users.noreply.github.com>
2023-12-11 16:16:39 +0500

    Update _Antispam.js

--- plugins/_Antispam.js
+++ plugins/_Antispam.js
@@ -1,63 +1,56 @@
-//Thanks Chatgpt🤡
-import { performance } from 'perf_hooks';
-
-// This function is used to prevent users from sending too many messages in a chat application or bot.
-
-export async function before(m) {
-    // Get information about users and chats from global data.
+export async function before(m, { conn, isAdmin, isBotAdmin }) {
     const users = global.db.data.users;
     const chats = global.db.data.chats;
 
-    // Check various conditions to determine if anti-spam measures should be applied.
-
-    // If anti-spam is disabled for this chat, or if the message is from the bot itself,
-    // or if the message type is a system message or poll update, do nothing and exit the function.
     if (!chats[m.chat].antiSpam || m.isBaileys || m.mtype === 'protocolMessage' || m.mtype === 'pollUpdateMessage' || m.mtype === 'reactionMessage') {
         return;
     }
 
-    // If there is no message content, or if the sender is banned, or if the chat is banned,
-    // do nothing and exit the function.
     if (!m.msg || !m.message || m.key.remoteJid !== m.chat || users[m.sender].banned || chats[m.chat].isBanned) {
         return;
     }
 
-    // Create or access a 'spam' object for the sender to keep track of spam-related data.
     this.spam = this.spam || {};
     this.spam[m.sender] = this.spam[m.sender] || { count: 0, lastspam: 0 };
 
-    // Get the current time in milliseconds.
     const now = performance.now();
-
-    // Calculate the time difference since the last message from this sender.
     const timeDifference = now - this.spam[m.sender].lastspam;
 
-    // If the time difference is less than 10 seconds, it means the sender is sending messages too quickly.
+    console.log(`[Anti-Spam] Time difference: ${timeDifference}`);
+    console.log(`[Anti-Spam] Spam count: ${this.spam[m.sender].count}`);
+
     if (timeDifference < 10000) {
-        // Increment the sender's spam count.
         this.spam[m.sender].count++;
 
-        // If the sender's spam count reaches 5 or more, mark the sender as banned and set a 5-second cooldown.
+        console.log(`[Anti-Spam] Increased spam count: ${this.spam[m.sender].count}`);
+
         if (this.spam[m.sender].count >= 5) {
-            users[m.sender].banned = true;
-            this.spam[m.sender].lastspam = now + 5000;
+            console.log(`[Anti-Spam] User spam count exceeded. isAdmin: ${isAdmin}`);
 
-            // Schedule a timeout to unban the user and reset their spam count after 5 seconds.
-            setTimeout(() => {
-                users[m.sender].banned = false;
+            if (isAdmin) {
+                // If an admin is spamming, notify but do not remove (same message as before).
+                console.log(`[Anti-Spam] Admin is spamming. User not removed.`);
+                conn.reply(m.chat, `❗ *@${m.sender.split('@')[0]}* is spamming like there's no tomorrow, but they're an admin! 🚀🙉`, m);
+            } else {
+                // If a non-admin user is spamming, remove them (similar to antilink removal).
+                console.log(`[Anti-Spam] Non-admin user is spamming. Removing...`);
+
+                await conn.reply(
+                    m.chat,
+                    `🚫 Houston, I've detected a spammer! 👀\n\n*@${m.sender.split('@')[0]}* is floating away from this group. Over and out! 🚀🌎`,
+                    m
+                );
+
+                await conn.sendMessage(m.chat, { delete: m.key });
+                await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
+
                 this.spam[m.sender].count = 0;
-                m.reply(`✅ *Cooldown finished*\nYou can send messages again.`);
-            }, 5000);
-
-            // Notify the sender about the spamming and the remaining cooldown time.
-            const message = m.mtype.replace(/message$/i, '').replace('audio', m.msg.ptt ? 'PTT' : 'audio').replace(/^./, v => v.toUpperCase()) || 'Unknown';
-            return m.reply(`❌ *Please do not spam ${message}*\nWait for ${Math.ceil((this.spam[m.sender].lastspam - now) / 1000)} seconds`);
+                return;
+            }
         }
     } else {
-        // If the time difference is greater than or equal to 10 seconds, reset the sender's spam count.
         this.spam[m.sender].count = 0;
     }
 
-    // Update the 'lastspam' timestamp to the current time for the sender.
     this.spam[m.sender].lastspam = now;
 }
