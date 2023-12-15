b1ce23b0dad6833ec2f93b6e75f24d952a4a0d96
kashifcodebreaker <141224408+kashifcodebreaker@users.noreply.github.com>
2023-12-15 13:03:47 +0500

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -15,7 +15,8 @@ import fetch from "node-fetch";
 
 const defaultMenu = {
   before: `
-  %ucpn\n 🤖 *${botname} at Your Service!* 🚀
+  Hello @${m.sender.split("@")[0]}, %ucpn\n
+  🤖 *${botname} at Your Service!*\n
   
   ┏━━༻ *USER STATS* ༺━━┓
   ⚔️ *Name:* %name
@@ -39,7 +40,7 @@ const defaultMenu = {
   ╰──────────⳹
   %readmore
 `.trimStart(),
-  header: "┏━❀•🎀 *%category* 🎀•❀━┓",
+  header: "┏━❀•🎀 *%Category* 🎀•❀━┓",
   body: "◈ %cmd %isPremium %islimit",
   footer: "╚══•❅•°•❈•°•❅•══╝",
   after: "\n%me",
@@ -124,7 +125,7 @@ let handler = async (m, {
     let _text = [
       before,
       ...Object.keys(tags).map(tag => {
-        return header.replace(/%category/g, tags[tag]) + "\n" + [
+        return header.replace(/%Category/g, tags[tag].toUpperCase()) + "\n\n" + [
           ...help.filter(menu => menu.tags && menu.tags.includes(tag) && menu.help).map(menu => {
             return menu.help.map(help => {
               return body.replace(/%cmd/g, menu.prefix ? help : "%_p" + help)
