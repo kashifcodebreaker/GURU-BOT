c4a570885b8d347c9069cc567cb18d035dffa4a4
kashifcodebreaker <141224408+kashifcodebreaker@users.noreply.github.com>
2023-12-16 21:15:17 +0500

    Update Blackpink.js

--- plugins/Blackpink.js
+++ plugins/Blackpink.js
@@ -1,29 +1,22 @@
-import fetch from 'node-fetch'
+import fetch from 'node-fetch';
 
-let bpink = []
+let bpink = [];
 
 fetch('https://raw.githubusercontent.com/arivpn/dbase/master/kpop/blekping.txt')
-
     .then(res => res.text())
-
-    .then(txt => bpink = txt.split('\n'))
+    .then(txt => bpink = txt.split('\n'));
 
 let handler = async (m, { conn }) => {
+    let img = bpink[Math.floor(Math.random() * bpink.length)];
 
-    let img = bpink[Math.floor(Math.random() * bpink.length)]
+    if (!img) throw 'Oops! The Blackpink image collection is currently unavailable.';
 
-    if (!img) throw img
-
-    await conn.sendFile(m.chat, img, '', 'made by Guru', m, 0, { thumbnail: await (await fetch(img)).buffer() })
-
+    await conn.sendFile(m.chat, img, '', '🖼️ Enjoy this Blackpink moment!', m, 0, { thumbnail: await (await fetch(img)).buffer() });
 }
 
-handler.help = ['blackpink']
+handler.help = ['blackpink'];
+handler.tags = ['image'];
+handler.limit = false;
+handler.command = /^(bpink|bp|blackpink)$/i;
 
-handler.tags = ['image']
-
-handler.limit = false
-
-handler.command = /^(bpink|bp|blackpink)$/i
-
-export default handler
+export default handler;
