2cc5465d56df9d54c34673a7f5523301b819b6cc
kashifcodebreaker <141224408+kashifcodebreaker@users.noreply.github.com>
2023-12-16 15:35:29 +0500

    Update afk-afk.js

--- plugins/afk-afk.js
+++ plugins/afk-afk.js
@@ -1,19 +1,26 @@
 //import db from '../lib/database.js'
 
 let handler = async (m, { text, conn }) => {
-    let user = global.db.data.users[m.sender]
-    user.afk = + new Date
-    user.afkReason = text
+    let user = global.db.data.users[m.sender];
+    user.afk = +new Date;
+    user.afkReason = text;
     m.reply(`
-  😴 *AFK* 
-You are now afk until u send a message 
-▢ *User:* ${conn.getName(m.sender)} 
-▢ *Reason:* ${text ? text : ''}
-  `)
-}
-handler.help = ['afk <reason>']
-handler.tags = ['fun']
-handler.command = ['afk']
-handler.group = true
+  🌙 *AFK Mode Activated* 🌙
 
-export default handler
+🚪 Greetings, oh noble one! You have gracefully entered the serene realm of AFK.
+
+📅 You shall remain AFK until the cosmic forces deem it fitting for you to return. Fear not, for your absence is duly noted.
+
+👤 *User:* ${conn.getName(m.sender)}
+📜 *Reason:* ${text ? text : 'No specific reason provided'}
+
+📢 Remember, your return shall be celebrated with grandeur and fanfare. Until then, may your AFK journey be filled with cosmic wonders!
+  `);
+}
+
+handler.help = ['afk <reason>'];
+handler.tags = ['fun'];
+handler.command = ['afk'];
+handler.group = true;
+
+export default handler;
