47cd0cfda1c20092cbe6e0ba3dac47525b27a775
kashifcodebreaker <141224408+kashifcodebreaker@users.noreply.github.com>
2023-12-16 11:43:01 +0500

    Update Guru-nsfw.js

--- plugins/Guru-nsfw.js
+++ plugins/Guru-nsfw.js
@@ -1,28 +1,44 @@
-import fetch from 'node-fetch'
+import fetch from 'node-fetch';
 
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-  if (!global.db.data.chats[m.chat].nsfw) throw `🚫 group doesnt supprt nsfw \n\n enable it by \n*${usedPrefix}enable* nsfw`
-  let user = global.db.data.users[m.sender].age
-  if (user < 17) throw m.reply(`❎ uneed to be atleast 18 years`)
-m.react(rwait)
+let handler = async (m, { conn, usedPrefix, command }) => {
+  const groupChat = global.db.data.chats[m.chat];
+  const userAge = global.db.data.users[m.sender].age;
 
-let res = await fetch(`https://fantox-apis.vercel.app/${command}`)
-if (!res.ok) throw await res.text()
-let json = await res.json()
-if (!json.url) throw '❎ Error'
-conn.sendFile(m.chat, json.url, 'img.jpg', `✅ Random ${command}`, m)
-m.react(dmoji)
-}
-handler.help = ['genshin', 'swimsuit', 'schoolswimsuit', 'white', 'barefoot', 'touhou', 'gamecg', 'hololive', 'uncensored', 'sunglasses', 'glasses', 'weapon', 'shirtlift', 'chain', 'fingering', 'flatchest', 'torncloth', 'bondage', 'demon', 'wet', 'pantypull', 'headdress', 'headphone', 'tie', 'anusview', 'shorts','stokings', 'topless', 'beach', 'bunnygirl', 'bunnyear', 'idol', 'vampire', 'gun', 'maid', 'bra', 'nobra', 'bikini', 'whitehair', 'blonde', 'pinkhair', 'bed', 'ponytail', 'nude', 'dress', 'underwear', 'foxgirl', 'uniform', 'skirt', 'sex', 'sex2', 'sex3', 'breast', 'twintail', 'spreadpussy', 'tears', 'seethrough', 'breasthold', 'drunk', 'fateseries', 'spreadlegs', 'openshirt', 'headband', 'food', 'close', 'tree', 'nipples', 'erectnipples', 'horns', 'greenhair', 'wolfgirl', 'catgirl']
-handler.command = ['genshin', 'swimsuit', 'schoolswimsuit', 'white', 'barefoot', 'touhou', 'gamecg', 'hololive', 'uncensored', 'sunglasses', 'glasses', 'weapon', 'shirtlift', 'chain', 'fingering', 'flatchest', 'torncloth', 'bondage', 'demon', 'wet', 'pantypull', 'headdress', 'headphone', 'tie', 'anusview', 'shorts','stokings', 'topless', 'beach', 'bunnygirl', 'bunnyear', 'idol', 'vampire', 'gun', 'maid', 'bra', 'nobra', 'bikini', 'whitehair', 'blonde', 'pinkhair', 'bed', 'ponytail', 'nude', 'dress', 'underwear', 'foxgirl', 'uniform', 'skirt', 'sex', 'sex2', 'sex3', 'breast', 'twintail', 'spreadpussy', 'tears', 'seethrough', 'breasthold', 'drunk', 'fateseries', 'spreadlegs', 'openshirt', 'headband', 'food', 'close', 'tree', 'nipples', 'erectnipples', 'horns', 'greenhair', 'wolfgirl', 'catgirl']
-handler.tags = ['nsfw'] 
-handler.diamond = true
-handler.group = true
-handler.register = true
+  if (!groupChat.nsfw) {
+    throw `🚫 This group doesn't support NSFW content.\n\nEnable it by using *${usedPrefix}enable* nsfw`;
+  }
 
+  if (userAge < 17) {
+    throw `❎ You need to be at least 18 years old to access NSFW content.`;
+  }
 
-export default handler
+  m.reply(`⌛ Please wait while I find ${command.toLowerCase()} image for you...`);
+
+  try {
+    const res = await fetch(`https://fantox-apis.vercel.app/${command}`);
+    if (!res.ok) throw await res.text();
+
+    const json = await res.json();
+    if (!json.url) throw '❎ Error';
+
+    conn.sendFile(m.chat, json.url, 'img.jpg', `✅ Here's the ${command.toLowerCase()} image for you!`, m);
+    m.reply(`🔥 Enjoy!`);
+  } catch (error) {
+    console.error(error);
+    m.reply(`❎ Oops! Something went wrong while fetching the image.`);
+  }
+};
+
+handler.help = ['genshin', 'swimsuit', 'schoolswimsuit', 'white', 'barefoot', 'touhou', 'gamecg', 'hololive', 'uncensored', 'sunglasses', 'glasses', 'weapon', 'shirtlift', 'chain', 'fingering', 'flatchest', 'torncloth', 'bondage', 'demon', 'wet', 'pantypull', 'headdress', 'headphone', 'tie', 'anusview', 'shorts', 'stokings', 'topless', 'beach', 'bunnygirl', 'bunnyear', 'idol', 'vampire', 'gun', 'maid', 'bra', 'nobra', 'bikini', 'whitehair', 'blonde', 'pinkhair', 'bed', 'ponytail', 'nude', 'dress', 'underwear', 'foxgirl', 'uniform', 'skirt', 'sex', 'sex2', 'sex3', 'breast', 'twintail', 'spreadpussy', 'tears', 'seethrough', 'breasthold', 'drunk', 'fateseries', 'spreadlegs', 'openshirt', 'headband', 'food', 'close', 'tree', 'nipples', 'erectnipples', 'horns', 'greenhair', 'wolfgirl', 'catgirl'];
+handler.command = ['genshin', 'swimsuit', 'schoolswimsuit', 'white', 'barefoot', 'touhou', 'gamecg', 'hololive', 'uncensored', 'sunglasses', 'glasses', 'weapon', 'shirtlift', 'chain', 'fingering', 'flatchest', 'torncloth', 'bondage', 'demon', 'wet', 'pantypull', 'headdress', 'headphone', 'tie', 'anusview', 'shorts', 'stokings', 'topless', 'beach', 'bunnygirl', 'bunnyear', 'idol', 'vampire', 'gun', 'maid', 'bra', 'nobra', 'bikini', 'whitehair', 'blonde', 'pinkhair', 'bed', 'ponytail', 'nude', 'dress', 'underwear', 'foxgirl', 'uniform', 'skirt', 'sex', 'sex2', 'sex3', 'breast', 'twintail', 'spreadpussy', 'tears', 'seethrough', 'breasthold', 'drunk', 'fateseries', 'spreadlegs', 'openshirt', 'headband', 'food', 'close', 'tree', 'nipples', 'erectnipples', 'horns', 'greenhair', 'wolfgirl', 'catgirl'];
+handler.tags = ['nsfw'];
+handler.diamond = true;
+handler.group = true;
+handler.register = true;
+
+export default handler;
 
 function pickRandom(list) {
-  return list[Math.floor(list.length * Math.random())]
-}
+  return list[Math.floor(list.length * Math.random())];
+                }
+                   
