58434001cde3038c92361be21bd5228dda93186b
kashifcodebreaker <141224408+kashifcodebreaker@users.noreply.github.com>
2023-12-17 23:36:20 +0500

    Update gettopic.js

--- plugins/gettopic.js
+++ plugins/gettopic.js
@@ -4,14 +4,23 @@ import axios from 'axios';
 
 let handler = async (m) => {
   try {
-    // Fetch a random conversation topic from the API
-    const response = await axios.get('https://www.conversationstarters.com/getjson.php');
+    const options = {
+      method: 'GET',
+      url: 'https://conversation-starter1.p.rapidapi.com/',
+      headers: {
+        'X-RapidAPI-Key': '684ef826d8msh3f26c47180d354bp1d1d82jsn81ad8ea6a990',
+        'X-RapidAPI-Host': 'conversation-starter1.p.rapidapi.com',
+      },
+    };
+
+    // Fetch a random conversation topic from the RapidAPI
+    const response = await axios.request(options);
     const topic = response.data;
 
     // Reply with the fetched conversation topic
-    return m.reply(`🗣️ *Random Topic to Discuss:* ${topic}`);
+    return m.reply(`🗣️ *Random Topic:* ${topic}`);
   } catch (error) {
-    console.error('Error fetching topic from API:', error);
+    console.error('Error fetching topic from RapidAPI:', error);
     return m.reply('❌ Oops! Something went wrong. Unable to fetch a topic at the moment.');
   }
 };
