25824ab376f6ad145f09653970cbb6d62807ec07
kashifcodebreaker <141224408+kashifcodebreaker@users.noreply.github.com>
2023-12-18 17:11:20 +0500

    Update tools-wikipedia.js

--- plugins/tools-wikipedia.js
+++ plugins/tools-wikipedia.js
@@ -1,27 +1,83 @@
-import axios from 'axios'
-import cheerio from 'cheerio'
+// wikipedia.js
 
+import axios from 'axios';
 
-let handler = async (m, { text }) => {
-	if (!text) throw `✳️ Enter what you want to search for on Wikipedia` 
-	
-    try {
-	const link =  await axios.get(`https://es.wikipedia.org/wiki/${text}`)
-	const $ = cheerio.load(link.data)
-	let wik = $('#firstHeading').text().trim()
-	let resulw = $('#mw-content-text > div.mw-parser-output').find('p').text().trim()
-	m.reply(`▢ *Wikipedia*
+let handler = async (m, { args }) => {
+  try {
+    if (!args.length) {
+      return m.reply('❓ Come on! Throw me a bone - provide a search query to get Wikipedia information.');
+    }
 
-‣ Buscado : ${wik}
+    // Combine the arguments to form the search query
+    const query = args.join(' ');
 
-${resulw}`)
-} catch (e) {
-  m.reply('⚠️ No results found ')
-}
-}
-handler.help = ['wikipedia']
-handler.tags = ['tools']
-handler.command = ['wiki','wikipedia'] 
+    // Fetch data from the Wikipedia API
+    const response = await axios.get('https://en.wikipedia.org/w/api.php', {
+      params: {
+        action: 'query',
+        format: 'json',
+        prop: 'extracts|pageimages|revisions|linkshere',
+        exintro: true,
+        explaintext: true,
+        piprop: 'thumbnail',
+        pithumbsize: 400,
+        rvsection: 0,
+        lhnamespace: 0,
+        lhshow: '!redirect',
+        titles: query,
+      },
+    });
 
+    // Extract relevant information from the API response
+    const pages = response.data.query.pages;
+    const pageId = Object.keys(pages)[0];
+    const page = pages[pageId];
+    const content = page.extract;
+    const image = page.thumbnail?.source;
 
-export default handler
+    // Check if the search result is ambiguous (disambiguation)
+    const isDisambiguation = content && content.includes('may refer to:');
+
+    // Prepare additional information
+    const languageCode = 'en'; // Default language is English
+    const timestamp = new Date().toLocaleString();
+    const url = `https://${languageCode}.wikipedia.org/wiki/${encodeURIComponent(query)}`;
+
+    // Prepare related searches
+    const relatedSearches = isDisambiguation
+      ? '\n\n*Related Searches:* You might also want to check: ' +
+        content.split('may refer to:')[1].split('\n')[0].replace(/ *\([^)]*\) */g, '')
+      : '';
+
+    // Prepare a humorous touch
+    const humor = isDisambiguation
+      ? "\n\n🤔 *Feeling adventurous? Wikipedia suggests exploring these related searches!*"
+      : '';
+
+    // Prepare a rich response
+    const richResponse = `
+🌐 *Language:* ${languageCode}
+📚 *Title:* ${page.title}
+🔗 *Link:* ${url}
+🕒 *Timestamp:* ${timestamp}
+🔍 *Result:* ${content ? 'Found' : 'Not Found'}
+${image ? `\n🖼️ *Image:* ${image}` : ''} 
+${content ? content : `❌ No information found on Wikipedia.${humor}${relatedSearches}`}
+    `;
+
+    // Send the rich response
+    return m.reply(richResponse, { linkPreview: false });
+  } catch (error) {
+    console.error('Error fetching data from Wikipedia API:', error);
+    return m.reply(
+      '❌ Oops! Something went wrong. Unable to fetch Wikipedia information at the moment. Maybe try again later?'
+    );
+  }
+};
+
+handler.help = ['wiki <query>'];
+handler.tags = ['search', 'tool'];
+handler.command = ['wikipedia', 'wiki'];
+
+export default handler;
+	    
