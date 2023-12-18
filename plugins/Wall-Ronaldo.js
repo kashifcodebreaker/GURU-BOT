71d55b4450cdc5a1874da98140825d969844cbaf
kashifcodebreaker <141224408+kashifcodebreaker@users.noreply.github.com>
2023-12-18 15:35:18 +0500

    Update Wall-Ronaldo.js

--- plugins/Wall-Ronaldo.js
+++ plugins/Wall-Ronaldo.js
@@ -5,6 +5,6 @@ let ronaldo = await cristiano[Math.floor(cristiano.length * Math.random())]
 conn.sendFile(m.chat, ronaldo, 'error.jpg', `*siuuuuuuuuuu*`, m)}
 //conn.sendButton(m.chat, "*Siiiuuuuuu*", author, ronaldo, [['⚽ NEXT ⚽', `${usedPrefix + command}`]], m)}
 handler.help = ['cristianoronaldo', 'cr7']
-handler.tags = ['img']
+handler.tags = ['image']
 handler.command = /^(ronaldo|cr7)$/i
 export default handler
