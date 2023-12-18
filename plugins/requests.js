844528173daf8185f896955c19bbb5ff2f8d2a7c
kashifcodebreaker <141224408+kashifcodebreaker@users.noreply.github.com>
2023-12-18 11:08:57 +0500

    Update requests.js

--- plugins/requests.js
+++ plugins/requests.js
@@ -1,30 +1,26 @@
-import * as baileys from '@whiskeysockets/baileys';
-const groupRequestParticipantsList = baileys.groupRequestParticipantsList;
-const groupRequestParticipantsUpdate = baileys.groupRequestParticipantsUpdate;
+import pkg from '@whiskeysockets/baileys';
+const { makeGroupsSocket, groupRequestParticipantsList, groupRequestParticipantsUpdate, extractGroupMetadata } = pkg;
 
 let handler = async (m, { conn, args, isAdmin, isBotAdmin }) => {
     if (!isBotAdmin || !isAdmin || !m.isGroup) {
-        // Custom message provided in handler.js for non-admins or non-group context
         return;
     }
 
     const groupId = m.chat;
-
-    // Check if the command includes 'approve'
     const isApproveCommand = args[0]?.toLowerCase() === 'approve';
 
     if (!isApproveCommand) {
-        // Get the list of join requests
-        const responseList = await groupRequestParticipantsList(groupId);
+        try {
+            const responseList = await groupRequestParticipantsList(groupId);
 
-        if (responseList.length === 0) {
-            return m.reply('👥 No pending join requests. Your group is already as exclusive as a VIP club!');
-        }
+            if (responseList.length === 0) {
+                return m.reply('👥 No pending join requests. Your group is already as exclusive as a VIP club!');
+            }
 
-        const groupInfo = await conn.groupMetadata(groupId);
-        const numMembersNow = groupInfo.participants.length;
+            const groupInfo = await conn.groupMetadata(groupId);
+            const numMembersNow = groupInfo.participants.length;
 
-        return m.reply(`
+            return m.reply(`
 👥 There are ${responseList.length} pending join requests.
 👥 Current members in the group: ${numMembersNow}
 
@@ -33,10 +29,13 @@ let handler = async (m, { conn, args, isAdmin, isBotAdmin }) => {
 
 Example:
 *requeststojoingroup approve 3*
-        `);
+            `);
+        } catch (error) {
+            console.error('Error fetching join requests:', error);
+            return m.reply('❌ Error fetching join requests. Please try again later.');
+        }
     }
 
-    // Check if the number of members to approve is provided
     if (!args[1] || isNaN(args[1])) {
         return m.reply(`
 ❓ You forgot to tell me how many members you want to welcome to the party.
@@ -48,39 +47,42 @@ Example:
 
     const numToApprove = parseInt(args[1]);
 
-    // Get the list of join requests
-    const responseList = await groupRequestParticipantsList(groupId);
+    try {
+        const responseList = await groupRequestParticipantsList(groupId);
 
-    if (numToApprove > responseList.length) {
-        return m.reply(`
+        if (numToApprove > responseList.length) {
+            return m.reply(`
 ❌ You can't welcome more members than there are join requests.
 
 Total pending requests: ${responseList.length}
-        `);
-    }
+            `);
+        }
 
-    const membersToApprove = responseList.slice(0, numToApprove).map(req => req.jid);
+        const membersToApprove = responseList.slice(0, numToApprove).map(req => req.jid);
 
-    // Approve the requested members
-    const responseUpdate = await groupRequestParticipantsUpdate(groupId, membersToApprove, 'approve');
+        const responseUpdate = await groupRequestParticipantsUpdate(groupId, membersToApprove, 'approve');
 
-    if (responseUpdate.status === 200) {
-        const numApproved = membersToApprove.length;
-        const numLeft = responseList.length - numApproved;
+        if (responseUpdate.status === 200) {
+            const numApproved = membersToApprove.length;
+            const numLeft = responseList.length - numApproved;
 
-        const groupInfo = await conn.groupMetadata(groupId);
-        const numMembersNow = groupInfo.participants.length;
+            const groupInfo = await conn.groupMetadata(groupId);
+            const numMembersNow = groupInfo.participants.length;
 
-        return m.reply(`
+            return m.reply(`
 *🎉 Successfully welcomed ${numApproved} new member(s) to the party!* 🥳
 👤 Members welcomed: ${numApproved}
 👥 Members still waiting outside: ${numLeft}
 👥 Total members in the group now: ${numMembersNow}
 
 *Tip: The more, the merrier! Keep the party going! 🎊*
-        `);
-    } else {
-        return m.reply('❌ Failed to welcome new member(s). Maybe next time!');
+            `);
+        } else {
+            return m.reply('❌ Failed to welcome new member(s). Maybe next time!');
+        }
+    } catch (error) {
+        console.error('Error processing join requests:', error);
+        return m.reply('❌ Error processing join requests. Please try again later.');
     }
 };
 
@@ -92,4 +94,3 @@ handler.isAdmin = true;
 handler.isGroup = true;
 
 export default handler;
-
