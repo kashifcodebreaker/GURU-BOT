32c80f0160afec02d0b5226d15c941b5dd04de87
kashifcodebreaker <141224408+kashifcodebreaker@users.noreply.github.com>
2023-12-31 14:55:50 +0500

    Update _chatbot.js

--- plugins/_chatbot.js
+++ plugins/_chatbot.js
@@ -6,64 +6,63 @@ export async function before(m, { conn }) {
     if (m.isBaileys && m.fromMe) {
       return true;
     }
-    
-    if (!m.isGroup) {
-      return false;
-    }
+
+    // Removed the group check
+    // if (!m.isGroup) {
+    //    return false;
+    // }
 
     const users = global.db.data.users;
     const chats = global.db.data.chats;
 
     const user = global.db.data.users[m.sender];
     const chat = global.db.data.chats[m.chat];
-    let name = conn.getName(m.sender)
+    let name = conn.getName(m.sender);
+
     if (m.mtype === 'protocolMessage' || m.mtype === 'pollUpdateMessage' || m.mtype === 'reactionMessage' || m.mtype === 'stickerMessage') {
       return;
     }
 
-    if (!m.msg   || !m.message || m.key.remoteJid !== m.chat || users[m.sender].banned || chats[m.chat].isBanned) {
+    if (!m.msg || !m.message || m.key.remoteJid !== m.chat || users[m.sender].banned || chats[m.chat].isBanned) {
       return;
     }
 
-    if (!m.quoted ||!m.quoted.isBaileys) return
-
-    if (!chat.chatbot) { 
+    // Check if the chatbot is turned on
+    if (!chat.chatbot) {
       return true;
     }
-    
+
     const msg = encodeURIComponent(m.text);
-    console.log(msg)
-    
+    console.log(msg);
+
     const response = await axios.post('https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=AIzaSyDJC5a882ruaC4XL6ejY1yhgRkN-JNQKg8', {
-        contents: [{
-          parts: [{
-            text: msg
-          }]
+      contents: [{
+        parts: [{
+          text: msg
         }]
-      });
+      }]
+    });
 
     const data = response.data;
     if (data.candidates && data.candidates.length > 0) {
-        const candidate = data.candidates[0];
+      const candidate = data.candidates[0];
       const content = candidate.content;
 
-      
-      let reply = content.parts[0].text; 
+      let reply = content.parts[0].text;
       if (reply) {
         reply = reply.replace(/Google/gi, 'Silver Fox Inc, a Robotics Company');
         reply = reply.replace(/a large language model/gi, botname);
-    
-    m.reply(reply);
-        }
-    
-      } else {
-        
-        m.reply("That's a great question! I'd love to answer it, but I'm currently recharging my batteries. I'll be back at full power soon.");
-    
+
+        m.reply(reply);
       }
+
+    } else {
+
+      m.reply("That's a great question! I'd love to answer it, but I'm currently recharging my batteries. I'll be back at full power soon.");
+
+    }
   } catch (error) {
     console.log(error);
-    
+
   }
-            }
-      
+        }
