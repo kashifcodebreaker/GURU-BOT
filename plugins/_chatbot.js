e4d4925012c33a890e51e27c3a22fd8893a7b7e5
kashifcodebreaker <141224408+kashifcodebreaker@users.noreply.github.com>
2023-12-31 15:38:04 +0500

    Update _chatbot.js

--- plugins/_chatbot.js
+++ plugins/_chatbot.js
@@ -6,11 +6,10 @@ export async function before(m, { conn }) {
     if (m.isBaileys && m.fromMe) {
       return true;
     }
-
-    // Removed the group check
-    // if (!m.isGroup) {
-    //    return false;
-    // }
+    
+    if (!m.isGroup) {
+      return false;
+    }
 
     const users = global.db.data.users;
     const chats = global.db.data.chats;
@@ -23,46 +22,43 @@ export async function before(m, { conn }) {
       return;
     }
 
-    if (!m.msg || !m.message || m.key.remoteJid !== m.chat || users[m.sender].banned || chats[m.chat].isBanned) {
+    if (!m.msg   || !m.message || m.key.remoteJid !== m.chat || users[m.sender].banned || chats[m.chat].isBanned) {
       return;
     }
 
-    // Check if the chatbot is turned on
-    if (!chat.chatbot) {
+    if (!m.quoted || !m.quoted.isBaileys) return;
+
+    if (!global.db.data.settings[conn.user.jid].chatbot) {
       return true;
     }
-
+    
     const msg = encodeURIComponent(m.text);
     console.log(msg);
-
+    
     const response = await axios.post('https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=AIzaSyDJC5a882ruaC4XL6ejY1yhgRkN-JNQKg8', {
-      contents: [{
-        parts: [{
-          text: msg
+        contents: [{
+          parts: [{
+            text: msg
+          }]
         }]
-      }]
-    });
+      });
 
     const data = response.data;
     if (data.candidates && data.candidates.length > 0) {
       const candidate = data.candidates[0];
       const content = candidate.content;
 
-      let reply = content.parts[0].text;
+      let reply = content.parts[0].text; 
       if (reply) {
         reply = reply.replace(/Google/gi, 'Silver Fox Inc, a Robotics Company');
         reply = reply.replace(/a large language model/gi, botname);
-
+    
         m.reply(reply);
       }
-
     } else {
-
       m.reply("That's a great question! I'd love to answer it, but I'm currently recharging my batteries. I'll be back at full power soon.");
-
     }
   } catch (error) {
     console.log(error);
-
   }
-        }
+      }
