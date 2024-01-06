2129304d3d4b949214b283a145be131bfe0c4a14
kashifcodebreaker <141224408+kashifcodebreaker@users.noreply.github.com>
2024-01-07 02:25:18 +0500

    Update Guru-hentai.js

--- plugins/Guru-hentai.js
+++ plugins/Guru-hentai.js
@@ -1,20 +1,42 @@
-let handler = async (m, { conn, text, usedPrefix, command, args }) => {
-    if (!global.db.data.chats[m.chat].nsfw) throw `🚫 group doesnt supprt nsfw \n\n enable it by \n*${usedPrefix}enable* nsfw`
-    let user = global.db.data.users[m.sender].age
-    if (user < 17) throw m.reply(`❎ uneed to be atleast 18 years`)
-    if (!text) throw `*This command provides sauce from nhentai: ${usedPrefix + command} miku*`
+import fetch from 'node-fetch';
+
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+    if (!global.db.data.chats[m.chat].nsfw) 
+        throw `🚫 This group doesn't support NSFW content. Enable it with:\n*${usedPrefix}enable* nsfw`;
+
+    let userAge = global.db.data.users[m.sender].age;
+    if (userAge < 18) 
+        throw m.reply(`❎ You need to be at least 18 years old to access this command. Keep it safe!`);
+
+    if (!text) 
+        throw `*This command provides sauce from nhentai: ${usedPrefix + command} [search term]*`;
+
     try {
-    m.reply(global.wait)
-    let res = await fetch(`https://api.lolhuman.xyz/api/nhentaisearch?apikey=${lolkeysapi}&query=${text}`)    
-    let json = await res.json()
-    let aa = json.result[0].id
-    let aa2 = json.result[0].title_native
-    let res2 = await fetch(`https://api.lolhuman.xyz/api/nhentaipdf/${aa}?apikey=${lolkeysapi}`)
-    let json2 = await res2.json()
-    let aa3 = json2.result
-    await conn.sendMessage(m.chat, { document: { url: aa3 }, mimetype: 'application/pdf', fileName: `${aa2}.pdf` }, { quoted: m })
+        m.reply(global.wait);
+        
+        // Fetching nhentai search results
+        let res = await fetch(`https://api.lolhuman.xyz/api/nhentaisearch?apikey=${lolkeysapi}&query=${text}`);
+        let json = await res.json();
+
+        // Extracting relevant information from the results
+        let nhentaiId = json.result[0].id;
+        let nhentaiTitle = json.result[0].title_native;
+
+        // Fetching nhentai PDF
+        let res2 = await fetch(`https://api.lolhuman.xyz/api/nhentaipdf/${nhentaiId}?apikey=${lolkeysapi}`);
+        let json2 = await res2.json();
+        let nhentaiPDF = json2.result;
+
+        // Sending the nhentai PDF document
+        await conn.sendMessage(m.chat, { document: { url: nhentaiPDF }, mimetype: 'application/pdf', fileName: `${nhentaiTitle}.pdf` }, { quoted: m });
     } catch {
-    throw `*ERROR NOT FOUND TRY SEARCHING ANOTHER QUERY*`
-    }}
-    handler.command = /^(hentai)$/i
-    export default handler
+        throw `*ERROR: No results found. Try searching with a different query.*`;
+    }
+};
+
+handler.command = /^(hentai)$/i;
+handler.help = ['hentai'];
+handler.tags = ['nsfw'];
+handler.register = true;
+
+export default handler;
