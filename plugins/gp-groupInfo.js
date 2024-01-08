5133932693ea3134db2f75eaab434581cdb85055
kashifcodebreaker <141224408+kashifcodebreaker@users.noreply.github.com>
2024-01-09 00:07:25 +0500

    Update gp-groupInfo.js

--- plugins/gp-groupInfo.js
+++ plugins/gp-groupInfo.js
@@ -1,11 +1,18 @@
-//import db from '../lib/database.js'
+// import db from '../lib/database.js';
 
 let handler = async (m, { conn, participants, groupMetadata }) => {
-    const pp = await conn.getProfilePicture(m.chat).catch(_ => null) || './src/avatar_contact.png';
     const { isBanned, welcome, detect, sWelcome, sBye, sPromote, sDemote, antiLink, delete: del } = global.db.data.chats[m.chat];
     const groupAdmins = participants.filter(p => p.admin);
     const listAdmin = groupAdmins.map((v, i) => `  ${i + 1}. @${v.id.split('@')[0]}`).join('\n');
     const owner = groupMetadata.owner || groupAdmins.find(p => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net';
+
+    let pp;
+    try {
+        pp = await conn.getProfilePicture(m.chat).catch(_ => null) || './src/avatar_contact.png';
+    } catch (error) {
+        console.error('Error fetching profile picture:', error);
+        pp = './src/avatar_contact.png';
+    }
 
     let text = `
 🌐 *Group Information* 🌐
@@ -19,7 +26,7 @@ let handler = async (m, { conn, participants, groupMetadata }) => {
 *👥 Members Count:*
 • ${participants.length}
 
-*👮🏻 Group's Super Admin:*
+*🤿 Group's Super Admin:*
 • @${owner.split('@')[0]}
 
 *🕵️‍♂️ Admins:*
@@ -39,9 +46,7 @@ ${listAdmin}
 • Demoted: ${sDemote}
 `.trim();
 
-    const fileType = pp.endsWith('.gif') ? 'gif' : (pp.endsWith('.png') ? 'png' : 'jpeg');
-
-    conn.sendFile(m.chat, pp, 'group_info.' + fileType, text, m, false, { mentions: [...groupAdmins.map(v => v.id), owner] });
+    conn.sendFile(m.chat, pp, 'group_info.jpg', text, m, false, { mentions: [...groupAdmins.map(v => v.id), owner] });
 };
 
 handler.help = ['groupinfo'];
