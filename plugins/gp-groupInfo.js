248096613b0a75564e9b7aa5677bff8cf4d7f835
kashifcodebreaker <141224408+kashifcodebreaker@users.noreply.github.com>
2024-01-08 19:14:52 +0500

    Update gp-groupInfo.js

--- plugins/gp-groupInfo.js
+++ plugins/gp-groupInfo.js
@@ -1,11 +1,11 @@
 //import db from '../lib/database.js'
 
 let handler = async (m, { conn, participants, groupMetadata }) => {
-    const pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null) || './src/avatar_contact.png';
-    const { isBanned, welcome, detect, sWelcome, sBye, sPromote, sDemote, antiLink, delete: del } = global.db.data.chats[m.chat];
-    const groupAdmins = participants.filter(p => p.admin);
-    const listAdmin = groupAdmins.map((v, i) => `  ${i + 1}. @${v.id.split('@')[0]}`).join('\n');
-    const owner = groupMetadata.owner || groupAdmins.find(p => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net';
+    const pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null) || './src/avatar_contact.png'
+    const { isBanned, welcome, detect, sWelcome, sBye, sPromote, sDemote, antiLink, delete: del } = global.db.data.chats[m.chat]
+    const groupAdmins = participants.filter(p => p.admin)
+    const listAdmin = groupAdmins.map((v, i) => `  ${i + 1}. @${v.id.split('@')[0]}`).join('\n')
+    const owner = groupMetadata.owner || groupAdmins.find(p => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net'
 
     let text = `
 🌐 *Group Information* 🌐
@@ -28,7 +28,7 @@ ${listAdmin}
 🔧 *Group Settings:*
 • Banned: ${isBanned ? '✅' : '❎'}
 • Welcome: ${welcome ? '✅' : '❎'}
-• Detector: ${detect ? '✅' : '❎'}
+• Detector: ${detect ? '✅' or '❎'}
 • Anti Delete: ${del ? '❎' : '✅'}
 • Anti Link: ${antiLink ? '✅' : '❎'}
 
@@ -48,4 +48,4 @@ handler.command = ['groupinfo', 'gpinfo'];
 handler.group = true;
 
 export default handler;
-    
+              
